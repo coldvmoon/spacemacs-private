146a0702dd2535e3c8a4ca1f41f379e6c074bb06
xkwu <xkwu1990@gmail.com>
2017-03-05 14:50:28 +0800

    完成jekyll导出功能

--- xkwu/packages.el
+++ xkwu/packages.el
@@ -71,9 +71,39 @@ Each entry is either:
          (dot . t)
          (ditaa . t)
        )))
+(setq org-confirm-babel-evaluate nil)
   (setq org-ditaa-jar-path "~/.spacemacs.d/ditaa0_9.jar")
 	  (setq org-plantuml-jar-path "~/.spacemacs.d/plantuml.jar")
 
+	  
+	  (setq org-publish-project-alist
+      '(
+
+  ("org-blog"
+          ;; Path to your org files.
+          :base-directory "~/blog/coldvmoon.github.io/_drafts"
+          :base-extension "org"
+
+          ;; Path to your Jekyll project.
+          :publishing-directory "~/blog/coldvmoon.github.io/_posts"
+          :recursive t
+          :publishing-function org-html-publish-to-html
+          :headline-levels 4 
+          :html-extension "html"
+          :body-only t ;; Only export section between <body> </body>
+    )
+
+
+    ("org-static-blog"
+          :base-directory "~/blog/coldvmoon.github.io/_drafts"
+          :base-extension "css\\|js\\|png\\|jpg\\|gif\\|pdf\\|mp3\\|ogg\\|swf\\|php"
+          :publishing-directory "~/blog/coldvmoon.github.io/images"
+          :recursive t
+          :publishing-function org-publish-attachment)
+
+    ("blog" :components ("org-blog" "org-static-blog"))
+
+))
 
     
 	)
